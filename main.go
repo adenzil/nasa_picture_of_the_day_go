f2e30e58686a02a4e9c382de0bd0fc232c9b4f21
Alister <alistercabral@gmail.com>
2018-01-01 12:32:00 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Api_result struct {
+	Url             string `json:"url"`
+	Date            string `json:"date"`
+	HDurl           string `json:"hdurl"`
+	Title           string `json:"title"`
+	Copyright       string `json:"copyright"`
+	Media_type      string `json:"media_type"`
+	Explanation     string `json:"explanation"`
+	Service_version string `json:"service_version"`
+}
+
+var api_key string = "your_nasa_api_key_here"
+
+func main() {
+
+	url := fmt.Sprintf("https://api.nasa.gov/planetary/apod?api_key=%s&hd=true", api_key)
+
+	req, err := http.NewRequest("GET", url, nil)
+	check_error(err, "NewRequest : ")
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	check_error(err, "Do: ")
+
+	defer resp.Body.Close()
+
+	var data Api_result
+
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println(data)
+
+}
+
+func check_error(err error, msg string) {
+	if err != nil {
+		log.Fatal(msg, err)
+		return
+	}
+}
